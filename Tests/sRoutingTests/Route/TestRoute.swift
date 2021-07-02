562dd44927ea5e501e21a52fb755574031d95d4b
Thang Kieu <thangkm.lk@gmail.com>
2021-07-02 16:18:45 +0700

    test covers pr

---
+++ Tests/sRoutingTests/Route/TestRoute.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by ThangKieu on 7/2/21.
+//
+
+import SwiftUI
+import sRouting
+
+enum TestRoute: Route {
+    case test
+    
+    var screen: some View { EmptyView() }
+}
