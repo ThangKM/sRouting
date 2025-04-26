cd0829d8ad84d3b86b95c2e4eb3cc66e22457ffb
Thang Kieu <thang.k@rookieme.com>
2025-04-26 16:51:17 +0700

    clean code.

--- Sources/sRouting/Models/SRTransition.swift
+++ Sources/sRouting/Models/SRTransition.swift
@@ -10,10 +10,10 @@ import SwiftUI
 public struct SRTransition<Route>: Sendable
 where Route: SRRoute {
     
-    let contextId: TimeIdentifier
+    let contextId: TimeIdentifier = .init()
     let type: SRTransitionKind
-    
     let transaction: WithTransaction?
+    
     private(set) var route: Route?
     private(set) var alert: Route.AlertRoute?
     private(set) var tabIndex: (any IntRawRepresentable)?
@@ -25,41 +25,35 @@ where Route: SRRoute {
     
     init(switchTo route: some SRRoute, and transaction: WithTransaction? = .none) {
         self.type = .switchRoot
-        self.contextId = TimeIdentifier()
         self.rootRoute = route
         self.transaction = transaction
     }
     
     init(with route: Route.PopoverRoute?, and transaction: WithTransaction? = .none) {
         self.type = .popover
-        self.contextId = TimeIdentifier()
         self.popover = route
         self.transaction = transaction
     }
     
     init(with route: Route.ConfirmationDialogRoute?, and transaction: WithTransaction? = .none) {
         self.type = .confirmationDialog
-        self.contextId = TimeIdentifier()
         self.confirmationDialog = route
         self.transaction = transaction
     }
 
     init(with type: SRTransitionKind, and transaction: WithTransaction? = .none) {
         self.type = type
-        self.contextId = TimeIdentifier()
         self.transaction = transaction
     }
     
     init(selectTab tab: any IntRawRepresentable, and transaction: WithTransaction? = .none) {
         self.type = .selectTab
-        self.contextId = TimeIdentifier()
         self.tabIndex = tab
         self.transaction = transaction
     }
 
     init(with alert: Route.AlertRoute, and transaction: WithTransaction? = .none) {
         self.type = .alert
-        self.contextId = TimeIdentifier()
         self.alert = alert
         self.transaction = transaction
     }
@@ -67,21 +61,18 @@ where Route: SRRoute {
     init(with route: Route, and action: SRTransitionKind, transaction: WithTransaction? = .none) {
         self.type = action
         self.route = route
-        self.contextId = TimeIdentifier()
         self.transaction = transaction
     }
     
     init(popTo path: some StringRawRepresentable, and transaction: WithTransaction? = .none) {
         self.type = .popToRoute
         self.popToPath = path
-        self.contextId = TimeIdentifier()
         self.transaction = transaction
     }
     
     init(with type: SRTransitionKind,
                 windowTransition: SRWindowTransition,
                 transaction: WithTransaction? = .none) {
-        self.contextId = TimeIdentifier()
         self.type = type
         self.windowTransition = windowTransition
         self.transaction = transaction
